6a0a86e65d04d1333be3e4b79a93765e8610cc96
NathalieOwusu <salem.moon@icloud.com>
2025-03-04 01:25:32 -0500

    Updated styles and components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-function Footer(){
-    return(
-        <footer>
+
+function Footer() {
+    return (
+        <footer className="position-absolute bottom-0 w-100 text-center text-white" style={{ zIndex: 100, padding: '10px 0' }}>
             <p>&#169; 2025 Rock N Rooster Saloon. All rights reserved. </p>
-                <p>Created by Nathalie Owusu </p>
+            <p>Created by Nathalie Owusu</p>
         </footer>
-    )
+    );
 }
 
 export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import HeroVideo from "../images/HeroVideo.mp4";
+import { Link } from 'react-router-dom';
+
 function Home(){
     return(
         <div>
-            <h1>
-                Welcome to Rock 'N' Rooster Saloon
-            </h1>
-            <p>
-                Experience the best live music, drinks, and food in town!
-            </p>
+            <div className= "position-relative">
+                <video 
+                autoPlay 
+                muted 
+                loop 
+                className="w-100 vh-100 object-fit-cover">
+                    <source src={HeroVideo} type="video/mp4" />
+                </video>
+                <div className="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-50"></div>
+                {/* Content */}
+                <div className="position-absolute top-50 start-50 translate-middle text-center text-white">
+                    <h1 className="display-3 fw-bold">Welcome to Rock 'N' Rooster Saloon</h1>
+                    <p className="lead">Experience the best live music, drinks, and food in town!</p>
+                    <Link to="/reservations" >
+                    <button  className="border-0 bg-danger text-white rounded-pill px-4 py-2 shadow-sm hover:bg-danger-dark transition-all position-relative" style={{ margin:'0px', top:'45px' }}> Book Now</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
